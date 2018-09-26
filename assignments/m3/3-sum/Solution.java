d79512b7d5d0878d006477b659e23a8167a848e3
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-26 18:30:29 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- assignments/m3/3-sum/Solution.java
+++ assignments/m3/3-sum/Solution.java
@@ -12,7 +12,7 @@ class Solution {
 	}
 	public int threeSum(long[] array) {
 		int count = 0;
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0; i < array.length-1; i++) {
 			long k = array[i] + array[i + 1];
 			for (int j = i; j < array.length; j++) {
 				if (k + array[j] == 0) {

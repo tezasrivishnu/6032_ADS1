7ddb05effec4cc959e30bf7283b052cf3b93952a
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-26 13:50:03 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- assignments/m3/Sorted_Array/Solution.java
+++ assignments/m3/Sorted_Array/Solution.java
@@ -1,52 +1,68 @@
 import java.util.Scanner;
 import java.util.Arrays;
 class Solution {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Solution sol = new Solution();
         Scanner sc = new Scanner(System.in);
         int firstSize = sc.nextInt();
         int secondSize = sc.nextInt();
         String[] firstarr = sc.next().split(",");
         String[] secondarr = sc.next().split(",");
-        int[] sorted = sol.sortedArray(firstSize, secondSize, firstarr, secondarr);
-        System.out.println(sol.toString(sorted));
+        int[] firstarray = new int[firstSize];
+        int[] secondarray = new int[secondSize];
+        for (int i = 0; i < firstSize; i++) {
+            firstarray[i] = Integer.parseInt(firstarr[i]);
+        }
+        for (int i = 0; i < secondSize; i++) {
+            secondarray[i] = Integer.parseInt(secondarr[i]);
+        }
+        if (firstSize == 0) {
+            sol.toString(secondarray);
+        }
+        if (secondSize == 0) {
+            sol.toString(firstarray);
+        } else {
+            int[] sorted = sol.sortedArray(firstSize, secondSize, firstarray, secondarray);
+            System.out.println(sol.toString(sorted));
+        }
     }
-    public int[] sortedArray(int onesize, int twosize, String[] onearray, String[] twoarray) {
-        int[] firstarray = new int[onesize];
-        int[] secondarray = new int[twosize];
-        for (int i = 0; i < onesize; i++) {
-            firstarray[i] = Integer.parseInt(onearray[i]);
-        }
-        for (int i = 0; i < twosize; i++) {
-            secondarray[i] = Integer.parseInt(twoarray[i]);
-        }
+    public int[] sortedArray(final int onesize, final int twosize,
+                             final int[] onearray, final int[] twoarray) {
+        // int[] firstarray = new int[onesize];
+        // int[] secondarray = new int[twosize];
+        // for (int i = 0; i < onesize; i++) {
+        //     firstarray[i] = Integer.parseInt(onearray[i]);
+        // }
+        // for (int i = 0; i < twosize; i++) {
+        //     secondarray[i] = Integer.parseInt(twoarray[i]);
+        // }
         int[] sortedArray = new int[onesize + twosize];
         int index = 0;
         int firstindex = 0;
         int secondindex = 0;
         if (onearray.length == 0) {
-            sortedArray = secondarray;
+            sortedArray = twoarray;
         }
         if (twoarray.length == 0) {
-            sortedArray = firstarray;
+            sortedArray = onearray;
         } else {
             while (firstindex < onesize && secondindex < twosize) {
-                if (firstarray[firstindex] < secondarray[secondindex]) {
-                    sortedArray[index] = firstarray[firstindex];
+                if (onearray[firstindex] < twoarray[secondindex]) {
+                    sortedArray[index] = onearray[firstindex];
                     firstindex++;
                 } else {
-                    sortedArray[index] = secondarray[secondindex];
+                    sortedArray[index] = twoarray[secondindex];
                     secondindex++;
                 }
                 index += 1;
             }
             while (firstindex < onesize) {
-                sortedArray[index] = firstarray[firstindex];
+                sortedArray[index] = onearray[firstindex];
                 firstindex += 1;
                 index += 1;
             }
             while (secondindex < twosize) {
-                sortedArray[index] = secondarray[secondindex];
+                sortedArray[index] = twoarray[secondindex];
                 secondindex += 1;
                 index += 1;
             }
@@ -54,7 +70,7 @@ class Solution {
         //System.out.println(Arrays.toString(sortedArray));
         return sortedArray;
     }
-    public String toString(int[] array) {
+    public String toString(final int[] array) {
         String s = "";
         int i = 0;
         for (i = 0; i < array.length - 1; i++) {

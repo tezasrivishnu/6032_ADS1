59b1d900f645737339bd3205280c6dafa3643d36
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-26 19:44:46 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m3/3-sum/Solution.java
+++ assignments/m3/3-sum/Solution.java
@@ -1,37 +1,53 @@
 import java.util.Scanner;
 import java.util.Arrays;
+/**
+ * Class for solution.
+ * @author tezasrivishnu
+ */
 class Solution {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Solution sol = new Solution();
-		int input = sc.nextInt();
-		long[] inputarray = new long[input];
-		for (int i = 0; i < input; i++) {
-			inputarray[i] = sc.nextLong();
-		}
-		System.out.println(sol.threeSum(inputarray));
-	}
-	public int threeSum(long[] array) {
-		Arrays.sort(array);
-		int count = 0;
-		for (int i = 0; i < array.length - 2; i++) {
-			// long a = array[i];
-			int j = i + 1;
-			int k = array.length - 1;
-			while (j < k) {
-				// long b = array[j];
-				// long c = array[k];
-				if(array[i] + array[j] + array[k] == 0) {
-					count += 1;
-					j += 1;
-					k -= 1;
-				} else if (array[i] + array[j] + array[k] > 0) {
-					k -= 1;
-				} else {
-					j += 1;
-				}
-			}
-		}
-		return count;
-	}
+    /**
+     * main program for the class solution.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Solution sol = new Solution();
+        int input = sc.nextInt();
+        long[] inputarray = new long[input];
+        for (int i = 0; i < input; i++) {
+            inputarray[i] = sc.nextLong();
+        }
+        System.out.println(sol.threeSum(inputarray));
+    }
+    /**
+     * finding if sum of three elements is zero using 3-sum complexity
+     *
+     * @param      array  the input array.
+     *
+     * @return     the count of the pairs whose sum is zero.
+     */
+    public int threeSum(final long[] array) {
+        Arrays.sort(array);
+        int count = 0;
+        for (int i = 0; i < array.length - 2; i++) {
+            long a = array[i];
+            int j = i + 1;
+            int k = array.length - 1;
+            while (j < k) {
+                long b = array[j];
+                long c = array[k];
+                if (a + b + c == 0) {
+                    count += 1;
+                    j += 1;
+                    k -= 1;
+                } else if (a + b + c > 0) {
+                    k -= 1;
+                } else {
+                    j += 1;
+                }
+            }
+        }
+        return count;
+    }
 }

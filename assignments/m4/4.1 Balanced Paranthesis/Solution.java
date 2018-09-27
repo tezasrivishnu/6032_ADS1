c07ee622a7986364f3cd179e0f4815297705e03a
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-27 18:02:20 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- assignments/m4/4.1 Balanced Paranthesis/Solution.java
+++ assignments/m4/4.1 Balanced Paranthesis/Solution.java
@@ -5,106 +5,109 @@ import java.util.Scanner;
  * @author tezasrivishnu
  */
 class CharacterStack {
-	/**
-	 * declaring the chararcter array.
-	 */
-	private char[] character;
-	/**
-	 * declaring the int count variable.
-	 */
-	private int count;
-	/**
-	 * Constructs the object.
-	 *
-	 * @param      size  The size
-	 */
-	public CharacterStack(final int size) {
-		character = new char[size];
-		count = 0;
-	}
-	/**
-	 * return the size of the array.
-	 *
-	 * @return     the size count.
-	 */
-	public int size() {
-		return count;
-	}
-	/**
-	 * getting the last element in array.
-	 *
-	 * @return     char element.
-	 */
-	public char pop() {
-		if (count > 0) {
-			char result = character[count--];
-			return result;
-		}
-		return ' ';
-	}
-	/**
-	 * adding a element to array.
-	 *
-	 * @param      c     the input character.
-	 */
-	public void push(final char c) {
-		character[count++] = c;
-	}
+ /**
+  * declaring the chararcter array.
+  */
+ private char[] character;
+ /**
+  * declaring the int count variable.
+  */
+ private int count;
+ /**
+  * Constructs the object.
+  *
+  * @param      size  The size
+  */
+ public CharacterStack(final int size) {
+  character = new char[size];
+  count = 0;
+ }
+ /**
+  * return the size of the array.
+  *
+  * @return     the size count.
+  */
+ public int size() {
+  return count;
+ }
+ /**
+  * getting the last element in array.
+  *
+  * @return     char element.
+  */
+ public char pop() {
+  if (count > 0) {
+   char result = character[count--];
+   return result;
+  }
+  return ' ';
+ }
+ /**
+  * adding a element to array.
+  *
+  * @param      c     the input character.
+  */
+ public void push(final char c) {
+  character[count++] = c;
+ }
 }
 /**
  * Class for solution.
  */
 class Solution {
-	/**
-	 * main class for the solution class.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int num = scan.nextInt();
-		Solution sol = new Solution();
-		for (int i = 0; i < num; i++) {
-			String input = scan.next();
-			sol.yesNo(input);
-		}
-	}
-	public void yesNo(final String value) {
-		CharacterStack stack = new CharacterStack(value.length());
-		char[] string = value.toCharArray();
-		int flag = 0;
-		if (string[0] == '}' || string[0] == ']' || string[0] == ')') {
-			System.out.println("NO");
-		} else {
-			if (string[string.length - 1] == '{' || string[string.length - 1] == '[' || string[string.length - 1] == '(') {
-				System.out.println("NO");
-			} else {
-				for (int c = 0; c < string.length; c++) {
-					if (string[c] == '[' || string[c] == '{' || string[c] == '(') {
-						stack.push(string[c]);
-					} else {
-						char s = stack.pop();
-						if (s == ' ') {
-							flag = 1;
-							break;
-						} else {
-							if (string[c] == '(' && s == ')') {
-								continue;
-							} else if (string[c] == '[' && s == ']') {
-								continue;
-							} else if (string[c] == '{' && s == '}') {
-								continue;
-							}
-						}
-					}
-				}
-				if (flag == 0 && stack.size() == 0) {
-					System.out.println("YES");
-				} else {
-					System.out.println("NO");
-				}
-			}
-		}
+ /**
+  * main class for the solution class.
+  *
+  * @param      args  The arguments
+  */
+ public static void main(final String[] args) {
+  Scanner scan = new Scanner(System.in);
+  int num = scan.nextInt();
+  Solution sol = new Solution();
+  for (int i = 0; i < num; i++) {
+   String input = scan.next();
+   sol.yesNo(input);
+  }
+ }
+ public void yesNo(final String value) {
+  CharacterStack stack = new CharacterStack(value.length());
+  char[] string = value.toCharArray();
+  int flag = 0;
+  if (string[0] == '}' || string[0] == ']' || string[0] == ')') {
+   System.out.println("NO");
+  } else {
+   if (string[string.length - 1] == '{'
+    || string[string.length - 1] == '['
+    || string[string.length - 1] == '(') {
+    System.out.println("NO");
+   } else {
+    for (int c = 0; c < string.length; c++) {
+     if (string[c] == '[' || string[c] == '{'
+      || string[c] == '(') {
+      stack.push(string[c]);
+     } else {
+      char s = stack.pop();
+      if (s == ' ') {
+       flag = 1;
+       break;
+      } else {
+       if (string[c] == '(' && s == ')') {
+        continue;
+       } else if (string[c] == '[' && s == ']') {
+        continue;
+       } else if (string[c] == '{' && s == '}') {
+        continue;
+       }
+      }
+     }
+    }
+    if (flag == 0 && stack.size() == 0) {
+     System.out.println("YES");
+    } else {
+     System.out.println("NO");
+    }
+   }
+  }
 
-	}
+ }
 }

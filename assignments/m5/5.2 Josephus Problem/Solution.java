92453ae263a78a271fa8e6cf88d906b39a0d72b3
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-29 19:13:56 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- assignments/m5/5.2 Josephus Problem/Solution.java
+++ assignments/m5/5.2 Josephus Problem/Solution.java
@@ -14,6 +14,7 @@ class Josephus {
             }
             System.out.print(queue.dequeue() + " ");
         }
+        System.out.print(queue.dequeue());
         System.out.println();
     }
 }

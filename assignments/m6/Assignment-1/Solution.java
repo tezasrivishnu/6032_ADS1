d77818216e837672c47ad75978ef36aaff845f9e
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-09-29 12:37:31 +0530

    "check_style_score -> 7 of 12 passed. style score: 0/1 "

--- assignments/m6/Assignment-1/Solution.java
+++ assignments/m6/Assignment-1/Solution.java
@@ -7,7 +7,7 @@ class AddLargeNumbers {
             str.push(input.charAt(i));
         }
         LinkedList<Character> str1 = new LinkedList<Character>();
-        while(!str.isEmpty()) {
+        while (!str.isEmpty()) {
             str1.push(str.pop());
         }
         return str1;
@@ -19,14 +19,53 @@ class AddLargeNumbers {
         //     string = string + list.pop();
         // }
         while (!(list.isEmpty())) {
-        string = string + list.pop();
+            string = string + list.pop();
         }
         return string;
     }
 
-    // public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
-
-    // }
+    public static LinkedList addLargeNumbers(LinkedList list1, LinkedList list2) {
+        LinkedList<Integer> res = new LinkedList<Integer>();
+        LinkedList<Character> one1 = list1;
+        LinkedList<Character> two1 = list2;
+        LinkedList<Character> one = new LinkedList<Character>();
+        LinkedList<Character> two = new LinkedList<Character>();
+        while(!one1.isEmpty()) {
+            one.push(one1.pop());
+        }
+        while(!two1.isEmpty()) {
+            two.push(two1.pop());
+        }
+        int quo1 = 0;
+        int result = 0;
+        while(!one.isEmpty() && !two.isEmpty()) {
+            char ch = one.pop();
+            char ch1 = two.pop();
+            int input = quo1 + Integer.parseInt(String.valueOf(ch)) + Integer.parseInt(String.valueOf(ch1)); 
+            //System.out.println("input "+input);
+            if(input >= 10) {
+                int reamin = input % 10;
+                int quo = input/10;
+                quo1 = quo;
+                result = reamin;
+                res.push(result);
+            } else {
+                result = input;
+                res.push(result);
+            }
+        }
+        while(!one.isEmpty()) {
+            char ch = one.pop();
+            result = quo1 + Integer.parseInt(String.valueOf(ch));
+            res.push(result);
+        }
+        while(!two.isEmpty()) {
+            char ch = two.pop();
+            result = quo1 + Integer.parseInt(String.valueOf(ch));
+            res.push(result);
+        }
+    return res;
+    }
 }
 
 public class Solution {
@@ -42,13 +81,13 @@ public class Solution {
             System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
             System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
             break;
-
-            // case "addLargeNumbers":
-            //     pDigits = AddLargeNumbers.numberToDigits(p);
-            //     qDigits = AddLargeNumbers.numberToDigits(q);
-            //     LinkedList result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
-            //     System.out.println(AddLargeNumbers.digitsToNumber(result));
-            //     break;
+        case "addLargeNumbers":
+            pDigits = AddLargeNumbers.numberToDigits(p);
+            qDigits = AddLargeNumbers.numberToDigits(q);
+            LinkedList result = 
+            AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
+            System.out.println(AddLargeNumbers.digitsToNumber(result));
+            break;
         }
     }
 

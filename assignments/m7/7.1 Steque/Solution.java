3b1dd33888f715e47cdd38b24ed80449b8ceb9e3
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-10-01 13:19:48 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- assignments/m7/7.1 Steque/Solution.java
+++ assignments/m7/7.1 Steque/Solution.java
@@ -50,7 +50,7 @@ class Steque {
 	}
 	public void print() {
 		if(size == 0) {
-			System.out.print("Steque is empty");
+			System.out.println("Steque is empty");
 			return;
 		}
 		String str = "";
@@ -64,7 +64,7 @@ class Steque {
 		// 	str += node.data +  ", ";
 		// }
 		str += node.data;
-		System.out.print(str);
+		System.out.println(str);
 	}
 
 }

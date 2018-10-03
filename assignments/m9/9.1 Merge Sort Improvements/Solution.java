aaee07c2b12c8ed5694203f30f05d7e1b8b93916
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-10-03 20:26:19 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- assignments/m9/9.1 Merge Sort Improvements/Solution.java
+++ assignments/m9/9.1 Merge Sort Improvements/Solution.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-import java.util.Arrays;
 class Solution {
 	public static void main(String[] args) {
 		Merge merge = new Merge();
@@ -19,52 +18,49 @@ class Merge {
 		insertion  = new Insertion();
 	}
 	public void sort(Comparable[] a) {
-		aux = new Comparable[a.length];
-		for (int i = 0; i < a.length; i++) {
-			aux[i] = a[i];
-		}
+		aux = a.clone();
 		sort(aux, a, 0, a.length - 1);
 		assert isSorted(a);
 	}
-	public void merge(Comparable[] aux, Comparable[] a, int lo, int mid, int hi) {
-		assert isSorted(aux, lo, mid);
-		assert isSorted(aux, mid + 1, hi);
+	public void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
+		assert isSorted(a, lo, mid);
+		assert isSorted(a, mid + 1, hi);
 		int i = lo;
 		int j = mid + 1;
 		for (int k = lo; k <= hi; k++) {
 			if (i > mid) {
-				a[k] = aux[j++];
+				aux[k] = a[j++];
 			} else if (j > hi) {
-				a[k] = aux[i++];
+				aux[k] = a[i++];
 			} else if (insertion.less(a[j], a[i])) {
-				a[k] = aux[j++];
+				aux[k] = a[j++];
 			} else {
-				a[k] = aux[i++];
+				aux[k] = a[i++];
 			}
 		}
-		assert isSorted(a, lo, hi);
+		assert isSorted(aux, lo, hi);
 	}
-	private void sort(Comparable[] aux, Comparable[] a, int lo, int hi) {
+	private void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
 		int cutoff = 7;
 		// if (hi <= lo) {
 		// 	return;
 		// }
 		if (hi <= lo + cutoff) {
-			a = insertion.sorting(a, lo, hi);
+			insertion.sorting(a, lo, hi);
 			System.out.println("Insertion sort method invoked...");
 			return;
 		}
 		int mid = lo + (hi - lo) / 2;
-		sort(a, aux, lo, mid);
-		sort(a, aux, mid + 1, hi);
-		if (!insertion.less(aux[mid + 1], aux[mid])) {
+		sort(aux, a, lo, mid);
+		sort(aux, a, mid + 1, hi);
+		if (!insertion.less(a[mid + 1], a[mid])) {
 			for (int i = lo; i <= hi; i++) {
-				a[i] = aux[i];
+				aux[i] = a[i];
 			}
 			System.out.println("Array is already sorted. So, skipped the call to merge...");
 			return;
 		}
-		merge(aux, a, lo, mid, hi);
+		merge(a, aux, lo, mid, hi);
 	}
 	public String toString(Comparable[] input) {
 		int i = 0;
@@ -89,7 +85,7 @@ class Merge {
 	}
 }
 class Insertion {
-	public Comparable[] sorting(Comparable[] team, int lo, int hi) {
+	public void sorting(Comparable[] team, int lo, int hi) {
 		// int[] teams = team;
 		// for (int i = lo; i < hi; i++) {
 		// 	int key = teams[i];
@@ -103,17 +99,15 @@ class Insertion {
 		// }
 		// return teams;
 		// for (int i = lo; i <= hi; i++)
-		Comparable[] sort = team;
+		// Comparable[] sort = team;
 		for (int i = lo; i <= hi; i++)
-			for (int j = i; j > lo && less(sort[j], sort[j - 1]); j--)
-				sort = exch(sort, j, j - 1);
-		return sort;
+			for (int j = i; j > lo && less(team[j], team[j - 1]); j--)
+				exch(team, j, j - 1);
 	}
-	private Comparable[] exch(Comparable[] a, int i, int j) {
+	private void exch(Comparable[] a, int i, int j) {
 		Comparable swap = a[i];
 		a[i] = a[j];
 		a[j] = swap;
-		return a;
 	}
 	public boolean less(final Comparable one, final Comparable two) {
 		return one.compareTo(two) < 0;

9895a1ae3176203b681e058a762fe31b49582deb
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-10-11 18:24:11 +0530

    "ads1_m16_a1: 2/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m16/16.1 BST/Solution.java
+++ assignments/m16/16.1 BST/Solution.java
@@ -1,99 +1,203 @@
 import java.util.Scanner;
+/**
+ * class for solution.
+ */
 final class Solution {
-	private Solution() {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
 
-	}
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		BinaryTree bt = new BinaryTree();
-		for (int i = 0; scan.hasNext(); i++) {
-			String[] tokens = scan.nextLine().split(",");
-			if (tokens[0].equals("put")) {
-				bt.put(new BookDetails(tokens[1], tokens[2],
-				                       Float.parseFloat(tokens[3])),
-				Integer.parseInt(tokens[4]));
-			} else if (tokens[0].equals("get")) {
-				System.out.println(bt.get(new BookDetails(tokens[1], tokens[2],
-				                       Float.parseFloat(tokens[3]))));
-			}
-		}
-	}
+    }
+    /**
+     * main method for the progrm.
+     * complexity O(n) as we are taking n inputs.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        BinaryTree bt = new BinaryTree();
+        for (int i = 0; scan.hasNext(); i++) {
+            String[] tokens = scan.nextLine().split(",");
+            if (tokens[0].equals("put")) {
+                bt.put(new BookInformtion(tokens[1], tokens[2],
+                                          Float.parseFloat(tokens[3])),
+                       Integer.parseInt(tokens[4]));
+            } else if (tokens[0].equals("get")) {
+                System.out.println(bt.get(new BookInformtion(tokens[1], tokens[2],
+                                          Float.parseFloat(tokens[3]))));
+            }
+        }
+    }
 }
-class BookDetails implements Comparable {
-	private String name;
-	private String author;
-	private float price;
-
-	BookDetails(String bookname, String bookauthor, float bookprice) {
-		this.name = bookname;
-		this.author = bookauthor;
-		this.price = bookprice;
-	}
-	public String getName() {
-		return this.name;
-	}
-	public String getAuthor() {
-		return this.author;
-	}
-	public float getPrice() {
-		return this.price;
-	}
-	public int compareTo(Object object) {
-		BookDetails that = (BookDetails) object;
-		return this.name.compareTo(that.name);
-	}
+/**
+ * Class for book informtion.
+ */
+class BookInformtion implements Comparable {
+    /**
+     * initalizing the string name.
+     */
+    private String name;
+    /**
+     * initalizing the string author.
+     */
+    private String author;
+    /**
+     * initalizing the float value price.
+     */
+    private float price;
+    /**
+     * Constructs the object.
+     *
+     * @param      bookname    The bookname
+     * @param      bookauthor  The bookauthor
+     * @param      bookprice   The bookprice
+     */
+    BookInformtion(final String bookname, final String bookauthor,
+                   final float bookprice) {
+        this.name = bookname;
+        this.author = bookauthor;
+        this.price = bookprice;
+    }
+    /**
+     * Gets the name of the book.
+     * complexity O(1) as we are just
+     * returning the name of book.
+     * @return     The name.
+     */
+    public String getName() {
+        return this.name;
+    }
+    /**
+     * Gets the author of the book.
+     * complexity O(1) as we are just
+     * returning the author of book.
+     * @return     The author.
+     */
+    public String getAuthor() {
+        return this.author;
+    }
+    /**
+     * Gets the price of the book.
+     * complexity O(1) as we are just
+     * returning the price of book.
+     * @return     The price.
+     */
+    public float getPrice() {
+        return this.price;
+    }
+    public int compareTo(final Object object) {
+        BookInformtion that = (BookInformtion) object;
+        return this.name.compareTo(that.name);
+    }
 }
+/**
+ * Class for node.
+ */
 class Node {
-	public BookDetails key;
-	public int val;
-	public Node left, right;
-	public Node(BookDetails key, int val) {
-		this.key = key;
-		this.val = val;
-		left = null;
-		right = null;
-	}
+
+    /**
+     * initiazing the bookinformation class object.
+     */
+    public BookInformtion key;
+    /**
+     * initiazing the int value.
+     */
+    public int val;
+    /**
+     * initiazing the node class objects.
+     */
+    public Node left, right;
+    /**
+     * Constructs the object.
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
+    public Node(final BookInformtion key,
+                final int val) {
+        this.key = key;
+        this.val = val;
+        left = null;
+        right = null;
+    }
 }
-
+/**
+ * Class for binary tree.
+ */
 class BinaryTree {
-	private Node root;
-	BinaryTree() {
-		root = null;
-	}
-	public Node root() {
-		return root;
-	}
-	public int get(BookDetails key) {
-		Node x = root;
-		while (x != null) {
-			int cmp = key.compareTo(x.key);
-			if (cmp < 0) {
-				x = x.left;
-			} else if (cmp > 0) {
-				x = x.right;
-			} else if (cmp == 0) {
-				return x.val;
-			}
-		}
-		return Integer.parseInt(null);
-	}
-	public void put(BookDetails key, int val) {
-		root = put(root, key, val);
-	}
-	private Node put(Node x, BookDetails key, int val) {
-		if (x == null) {
-			return new Node(key, val);
-		}
-		int cmp = key.compareTo(x.key);
-		if (cmp < 0) {
-			x.left = put(x.left, key, val);
-		} else if (cmp > 0) {
-			x.right = put(x.right, key, val);
-		} else if (cmp == 0) {
-			x.val = val;
-		}
-		return x;
-	}
-
-
+    /**
+     * initializing root node.
+     */
+    private Node root;
+    /**
+     * Constructs the object.
+     */
+    BinaryTree() {
+        root = null;
+    }
+    /**
+     *
+     * returning the root node of the tree.
+     * complexity O(1) we are just returing the value.
+     * @return     root node.
+     */
+    public Node root() {
+        return root;
+    }
+    /**
+     * getting the quantity of the book
+     * complexity O(n) as we might search
+     * the full tree to get the element.
+     * @param      key   The key
+     *
+     * @return     the quantity.
+     */
+    public int get(final BookInformtion key) {
+        Node x = root;
+        while (x != null) {
+            int cmp = key.compareTo(x.key);
+            if (cmp < 0) {
+                x = x.left;
+            } else if (cmp > 0) {
+                x = x.right;
+            } else if (cmp == 0) {
+                return x.val;
+            }
+        }
+        return Integer.parseInt(null);
+    }
+    /**
+     * helper method.
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
+    public void put(final BookInformtion key, int val) {
+        root = put(root, key, val);
+    }
+    /**
+     * inserting a element in the tree.
+     * complexity O(n) we must go through each and every parent node.
+     * @param      x     Node
+     * @param      key   The Book information key
+     * @param      val   The value
+     *
+     * @return     the node.
+     */
+    private Node put(final Node x,
+                     final BookInformtion key, final int val) {
+        if (x == null) {
+            return new Node(key, val);
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp < 0) {
+            x.left = put(x.left, key, val);
+        } else if (cmp > 0) {
+            x.right = put(x.right, key, val);
+        } else if (cmp == 0) {
+            x.val = val;
+        }
+        return x;
+    }
 }

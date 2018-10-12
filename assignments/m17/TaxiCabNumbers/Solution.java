ba4963f3c8df639ccafa4946ac61cd34490d89c0
tezasrivishnu <tezasri.vishnu@msitprogram.net>
2018-10-12 15:14:45 +0530

    "taxi_cab_numbers: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- assignments/m17/TaxiCabNumbers/Solution.java
+++ assignments/m17/TaxiCabNumbers/Solution.java
@@ -32,7 +32,7 @@ final class Solution {
             } else {
                 mcounter = 0;
             }
-            if (mcounter == mpairs - 1) {
+            if (mcounter == mpairs) {
                 nthnumber--;
                 if (nthnumber == 0) {
                     System.out.println(s.getSum());
